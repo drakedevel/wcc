8e72af25ae127dacfad574ffdda1529a86dcfece
Andrew Drake <adrake@adrake.org>
2011-04-07 08:29:53 -0400

    Add S-expression generation.

--- Language/Turing/Serialize.hs
+++ Language/Turing/Serialize.hs
@@ -1,32 +1,47 @@
 module Language.Turing.Serialize
        (
-         machineToDot
+         machineToDot,
+         machineToSexpr
        ) where
 
 import Control.Monad.Writer
+import Data.List
 import qualified Data.Set as S
 import Language.Turing.Machine
 import Text.Printf
 
-stateToDot :: (Show q) => q -> Writer String ()
-stateToDot x = tell $ printf "%s;\n" $ show x
+showPattern :: (TapeSymbol l) => Pattern l -> String
+showPattern p = "[" ++ (foldl (++) "" $ intersperse " " $ map showEnt p) ++ "]"
+  where showEnt (a,b) = show b ++ ":" ++ shortSymbol a
 
-ruleToDot :: (Show q, Show l) => Rule q l -> Writer String ()
+ruleToDot :: (Show q, TapeSymbol l) => Rule q l -> Writer String ()
 ruleToDot r = do
   tell $ show $ condState r
   tell " -> "
   tell $ show $ nextState r
   tell " [label=\""
-  tell $ show $ condPat r
-  tell " -> "
-  tell $ show $ actPat r
-  tell " -> "
+  tell $ showPattern $ condPat r
+  tell " / "
+  tell $ showPattern $ actPat r
+  tell " / "
   tell $ show $ delta r
   tell "\"];\n"
 
-machineToDot :: (Show q, Show l) => Machine q l -> String
+machineToDot :: (Show q, TapeSymbol l) => Machine q l -> String
 machineToDot m = execWriter $ do
   tell "digraph {\n"
-  mapM_ stateToDot $ S.toList $ states m
   mapM_ ruleToDot $ rules m
   tell "}\n"
+
+machineToSexpr :: (Enum q, Enum l) => Machine q l -> String
+machineToSexpr m =
+  let list s = '(' : (foldl (++) "" $ intersperse " " s) ++ ")"
+      ent (l, t) = list [show $ fromEnum $ l, show t]
+      pat p = list $ map ent p
+      rule r = list $ [show $ fromEnum $ condState r,
+                       show $ fromEnum $ nextState r,
+                       pat $ condPat r,
+                       pat $ actPat r]
+  in list [show $ fromEnum $ initState m,
+           show $ fromEnum $ haltState m,
+           list $ map rule $ rules m]
